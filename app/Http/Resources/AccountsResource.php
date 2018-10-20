3403de73cee3f8db86cc283e6c77125605970ce8
Jerwin Arnado <me@jackwander.pro>
2018-10-20 16:20:53 +0800

    Set return for AccountsResource.

--- app/Http/Resources/AccountsResource.php
+++ app/Http/Resources/AccountsResource.php
@@ -14,6 +14,20 @@ class AccountsResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "acct_id" => $this->acct_id,
+            "acct_name" => $this->acct_name,
+            "acct_detail" => $this->acct_detail,
+            "user_id" => $this->user_id,
+            "slug" => $this->slug,
+        ];
+    }
+
+    public function with($request) {
+        return [
+            'version' => '1.0.0',
+            'author_name' => 'Jerwin D. Arnado',
+            'author_url' => url('http://jackwander.pro')
+        ];
     }
 }
