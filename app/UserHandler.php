4f8d5ce41efe280c0c17281916f13c229afbb1b5
Jerwin Arnado <me@jackwander.pro>
2018-10-20 16:36:32 +0800

    Make Model: UserHandler.

---
+++ app/UserHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserHandler extends Model
+{
+    protected $table = 'users';
+
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'fname','mname','lname','contactnum', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function accounts() {
+        return $this->hasMany('App\Account');
+    }
+
+    public function transactions() {
+        return $this->hasMany('App\AccountTransaction');
+    }
+}
