77f8a965b20d68804d53dd4128d7a821b0b9f070
Jerwin Arnado <me@jackwander.pro>
2018-10-24 00:32:15 +0800

    Create Add form.

--- resources/assets/js/components/Accounts.vue
+++ resources/assets/js/components/Accounts.vue
@@ -4,7 +4,7 @@
     <div class="card">
       <div class="card-header">
         <div class="float-right">
-          <a href="#" data-toggle="modal" data-target="#addAccount"> <i class="fas fa-plus"></i> Add Account</a>
+          <a href="#" @click="addForm()" data-toggle="modal" data-target="#addAccount"> <i class="fas fa-plus"></i> Add Account</a>
         </div>
       </div>
       <div class="card-body d-sm-flex justify-content-sm-center">
@@ -77,6 +77,43 @@
         }
 
         this.pagination = pagination;
+      },
+      async addForm() {
+        let vm = this;
+        let formData = new FormData();
+        const {value: formValues} = await swal({
+          title: 'Add your account',
+          html:
+          '<input id="swal-input1" class="swal2-input" placeholder="Account Name">' +
+          '<input id="swal-input2" class="swal2-input" placeholder="Account Detail">',
+          focusConfirm: false,
+          preConfirm: () => {
+            return [
+            document.getElementById('swal-input1').value,
+            document.getElementById('swal-input2').value
+            ]
+          }
+        })
+
+        if (formValues) {
+
+          formData.append('user_id', vm.user_id);
+          formData.append('acct_name', formValues[0]);
+          formData.append('acct_detail', formValues[1]);
+
+          axios.post('/api/user/accounts/store', formData)
+          .then(function (response) {
+            swal({
+              type: 'success',
+              title: 'Congrats',
+              text: 'Your account is added!',
+            });
+            vm.fetchAccounts();
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        }
       }
     }
   }
