916a9ae3340716631c2ee39beddea77ef0bdf310
Jerwin Arnado <me@jackwander.pro>
2018-10-24 01:36:06 +0800

    Add route: View account.

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/', 'PageController@index')->name('dashboard');
 
 Route::prefix('accounts')->group(function() {
   Route::get('/', 'PageController@accounts')->name('show.accounts');
+  Route::get('/{slug}/', 'PageController@viewAccount')->name('view.account');
 });
